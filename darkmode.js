3ff639b439ef610b5883a746ab5d67e04ca5e94b
Kevin Chan <140776786+kevindkchan@users.noreply.github.com>
2024-08-28 22:22:26 -0600

    Create darkmode.js

---
+++ darkmode.js
@@ -0,0 +1,37 @@
+// divs:
+$("div").each(function () {
+    var color = $(this).css("color");
+    if (color == "rgb(50, 48, 47)") {
+        $(this).css("color", "#efefef");
+    }
+});
+
+// spans:
+$("span").each(function () {
+    var color = $(this).css("color");
+    if (color == "rgb(50, 48, 47)") {
+        $(this).css("color", "#efefef");
+    }
+});
+
+// ps:
+$("p").each(function () {
+    var color = $(this).css("color");
+    if (color == "rgb(72, 102, 53)") {
+        $(this).css("color", "#a9b883");
+    }
+});
+
+$("p").each(function () {
+    var color = $(this).css("color");
+    if (color == "rgb(164, 61, 18)") {
+        $(this).css("color", "#f98b73");
+    }
+});
+
+$("p").each(function () {
+    var color = $(this).css("color");
+    if (color == "rgb(50, 48, 47)") {
+        $(this).css("color", "#f1f1f1");
+    }
+});
